fa352048880f5ec47cd959553ebc43fe1bbc1862
David Fashbinder <david.fashbinder@gmail.com>
2020-12-02 16:19:44 -0500

    adding program files

--- app.py
+++ app.py
@@ -19,8 +19,6 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
-
-.update(query_parameter, data, options)
-
+   
 if __name__ == "__main__":
    app.run()
